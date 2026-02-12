e232bbe5520781279ead3c0ab60fe8a5be5d0b1e
nimithaajith <nimithaajith16@gmail.com>
2026-02-12 17:02:46 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,9 +8,11 @@ def main():
     # Uncomment the code below to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, _ =server_socket.accept() # wait for client
-    conn.sendall(b"+PONG\r\n")
-    conn.close()
+    while True :
+        conn, _ =server_socket.accept() # wait for client
+        conn.sendall(b"+PONG\r\n")
+
+    # conn.close()
 
 
 if __name__ == "__main__":
