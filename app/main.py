e9147bfd267391567952605c534724880d13881e
nimithaajith <nimithaajith16@gmail.com>
2026-02-12 17:04:34 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,8 +8,9 @@ def main():
     # Uncomment the code below to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    conn, _ =server_socket.accept() # wait for client        
     while True :
-        conn, _ =server_socket.accept() # wait for client
+        data = conn.recv(1024)
         conn.sendall(b"+PONG\r\n")
 
     # conn.close()
