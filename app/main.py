e21d2e4d627b8bb411ba149b2851b89cf91fa4bc
nimithaajith <nimithaajith16@gmail.com>
2026-03-11 17:47:33 +0530

    info replication of master with replid and offset

--- app/main.py
+++ app/main.py
@@ -681,7 +681,7 @@ async def client_handler(reader,writer):
                         print('sec2 =',sec2)
                         sec3='master_repl_offset:'+str(RedisAsyncServer.master_repl_offset)
                         print('sec3 =',sec3)
-                        master_resp=f'${len(sec2)}\r\n{sec2}\r\n{len(sec3)}\r\n{sec3}\r\n'
+                        master_resp=f'${len(sec2)}\r\n{sec2}\r\n${len(sec3)}\r\n{sec3}\r\n'
                         response = f'*3\r\n${length}\r\nrole:{role}\r\n' + master_resp
                     else:
                         response=f'${length}\r\nrole:{role}\r\n'
