977ac40ebdc59f0ab355524c02a798907113d0bb
nimithaajith <nimithaajith16@gmail.com>
2026-03-15 20:34:08 +0530

    PSYNC to master

--- app/main.py
+++ app/main.py
@@ -790,7 +790,7 @@ async def run_server(port_number):
                 m_writer.write(b'*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n')
                 await m_writer.drain()
                 data = await m_reader.readline()
-                if  data.decode()=='+OK':
+                if  'OK' in data.decode():
                     # sending PSYNC to master
                     response=f'*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n'
                     m_writer.write(response.encode())
