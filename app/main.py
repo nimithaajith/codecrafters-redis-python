9eb7fafad74724f0983fb7804b471e5e42e393f8
nimithaajith <nimithaajith16@gmail.com>
2026-04-03 12:58:15 +0530

    RDB Persistence Extension:stage1:config keys *

--- app/main.py
+++ app/main.py
@@ -820,7 +820,7 @@ async def client_handler(reader,writer):
             filepath=os.path.join(RedisAsyncServer.server.rdb_dir,RedisAsyncServer.server.rdb_filename)
             if os.path.exists(filepath):
                 with open(filepath,'rb') as rdbfile:
-                    print("<<<<<FILE EXISTS , reading....>>>>")
+                    print("<<<<<FILE EXISTS , reading....>>>>",filepath)
                     rdb_bytes=rdbfile.read()
                     json_str = rdb_bytes.decode()
                     RedisAsyncServer.data_store=json.loads(json_str)
