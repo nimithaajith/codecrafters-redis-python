78517456b21fec341c3a65c2f737afdf5bbe2656
nimithaajith <nimithaajith16@gmail.com>
2026-05-05 19:22:18 +0530

    Respond to ACL GETUSER #gx8

--- app/main.py
+++ app/main.py
@@ -1259,8 +1259,12 @@ async def command_handler(writer,client_addr,server_role,query_string,input_toke
                 response=f'*0\r\n'
             print("response = ",response)
 
-        elif data_list[0].upper() == 'ACL' and data_list[1].upper() == 'WHOAMI':
-            response = '$7\r\ndefault\r\n'
+        elif data_list[0].upper() == 'ACL':
+            if data_list[1].upper() == 'WHOAMI':
+                response = '$7\r\ndefault\r\n'
+            elif data_list[1].upper() == 'GETUSER':
+                user_name=data_list[2]
+                response = '*2\r\n$5\r\nflags\r\n$2\r\n*0\r\n'
         elif data_list[0] == 'TYPE': 
             key=data_list[1]
             if key in RedisAsyncServer.data_store.keys() :
