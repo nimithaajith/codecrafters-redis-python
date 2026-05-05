71d5afcd5bc5194258c62957e01d9a5fd7c39bc2
nimithaajith <nimithaajith16@gmail.com>
2026-05-05 13:09:55 +0530

    Replay a single command #xz2

--- app/main.py
+++ app/main.py
@@ -1730,8 +1730,10 @@ def main():
         if RedisAsyncServer.server.appendonly == 'yes':
             print(">>>>>>>>AOF FILE REPLAY<<<<<<<<")
             aof_path=os.path.join(RedisAsyncServer.server.dir,RedisAsyncServer.server.appenddirname)
+            print('aof_path =',aof_path)
             if os.path.exists(aof_path) :
                 manifest_file=os.path.join(aof_path,RedisAsyncServer.server.appendfilename,'manifest')
+                print("manifest_file = ",manifest_file)
                 if os.path.exists(manifest_file):
                     print("Opening manifest file")
                     with open(manifest_file,'r') as mf:
